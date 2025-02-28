5005cc98c8c0ccd604b700c64226f88270d6b976
Tomakiston <tomasmeme86@gmail.com>
2025-02-28 16:04:17 -0300

    second commit

--- game.js
+++ game.js
@@ -18,11 +18,14 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-let player;
+let player, enemy, thunders, cursors;
 let playerDirection = "right";
-let enemy;
-let thunders;
-let cursors;
+
+let score = 0;
+let playerLives = 3;
+let enemyLives = 5;
+
+let scoreText, livesText, enemyLivesText, victoryText;
 
 function preload() {
     this.load.image('flashRight', 'flashRight.png');
@@ -53,7 +56,8 @@ function create() {
 
     this.physics.add.collider(player, floor);
     this.physics.add.collider(enemy, floor);
-    //this.physics.add.overlap(thunders, enemy, hitEnemy, null, this);
+    this.physics.add.overlap(thunders, enemy, hitEnemy, null, this);
+    this.physics.add.overlap(enemy, thunders, hitEnemy, null, this);
 
     enemy.setCollideWorldBounds(true);
     enemy.setVelocityX(-1000);
@@ -62,15 +66,24 @@ function create() {
 
     cursors = this.input.keyboard.createCursorKeys();
     //this.input.keyboard.on('keydown-SPACE', playerThunders, this);
+
+    scoreText = this.add.text(16, 16, "Pontuação: 0", {fontSize: "20px", fontFamily: "Comic Sans MS",fill: "white"});
+    livesText = this.add.text(16, 45, "Vidas: 3", {fontSize: "20px", fontFamily: "Comic Sans MS",fill: "white"});
+    enemyLivesText = this.add.text(1293, 16, `Vida do Inimigo: ${enemyLives}`, {fontSize: "20px", fontFamily: "Comic Sans MS",fill: "red"});
+    victoryText = this.add.text(605, 310, "Você Venceu!", {fontSize: "50px", fontFamily: "Comic Sans MS", fill: "gold"}).setVisible(false);
 }
 function update() {
     if(cursors.left.isDown) {
         player.x -= 25;
         player.setTexture('flashLeft');
+
+        playerDirection = "left";
     }
     else if(cursors.right.isDown) {
         player.x += 25;
         player.setTexture('flashRight');
+
+        playerDirection = "right";
     }
     else if(cursors.up.isDown) {
         player.y -= 15;
@@ -93,12 +106,30 @@ function update() {
         if(thunder.active && (thunder.x < 0 || thunder.x > 1475)) thunder.setActive(false).setVisible(false);
     });
 }
-//Atualizar função playerThunders()
 function playerThunders() {
     const thunder = thunders.get(player.x, player.y - 20);
     if(thunder) {
         thunder.setActive(true).setVisible(true).setScale(0.15);
         thunder.body.allowGravity = false;
-        thunder.setVelocityX(600);
+
+        if(playerDirection === "right") {
+            thunder.setTexture("thunderRight");
+            thunder.setVelocityX(700);
+        }
+        else {
+            thunder.setTexture("thunderLeft");
+            thunder.setVelocityX(-700);
+        }
+    }
+}
+function hitEnemy(enemy, thunders) {
+    thunders.disableBody(true, true);
+    
+    enemyLives--;
+    enemyLivesText.setText(`Vida do Inimigo: ${enemyLives}`);
+    if(enemyLives <= 0) {
+        enemy.disableBody(true, true);
+        victoryText.setVisible(true);
+        thunders.clear(true, true);
     }
 }
